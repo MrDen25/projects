ba1190a797a79afed6b26c4e049dd989d6ed0bb0
MrDen25 <Kreativchik30@gmail.com>
2026-04-02 15:05:59 +0300

    main page

--- studio/js/script.js
+++ studio/js/script.js
@@ -60,16 +60,21 @@ if (heroSlider) {
 }
 "use strict"
 
+
+
 window.addEventListener('load', load)
+/* Перевірка мобільного браузера */
+const isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+/* Додавання класу touch для HTML, якщо браузер мобільний */
+function addTouchAttr() {
+    // Додавання data-fls-touch для HTML, якщо браузер мобільний
+    if (isMobile.any()) document.documentElement.setAttribute('data-fls-touch', '')
+}
+addTouchAttr()
 
 function load() {
-    /* Перевірка мобільного браузера */
-    const isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
-    /* Додавання класу touch для HTML, якщо браузер мобільний */
-    function addTouchAttr() {
-        // Додавання data-fls-touch для HTML, якщо браузер мобільний
-        if (isMobile.any()) document.documentElement.setAttribute('data-fls-touch', '')
-    }
+
+
     document.addEventListener('click', documentActions)
 
 
@@ -86,4 +91,10 @@ function load() {
 
         }
     }
+
 }
+
+
+
+
+
